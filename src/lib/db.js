9ab1177611c5dbaa53ff221d0b2e7ce969e54bd6
leoponzio <leoponzio1@gmail.com>
2020-09-20 10:03:59 -0400

    reset password

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        host: '192.160.10.37',
+        host: 'localhost',
         user: 'root',
         password: '',
         database: 'db_catalogo'
--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -3,9 +3,7 @@
 const helpers = {};
 
 helpers.isMaster = (arg1, arg2, options) => {
-        console.log(arg1);
-        console.log(arg2);
-        return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
  
 }
 
